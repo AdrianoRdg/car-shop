6a1b8f2c689547019ebcb4e5e9fb4339ee941320
Adriano <adrianoo.rdg@gmail.com>
2022-11-06 17:31:40 -0300

    feat: feito o controller do crud de motocycles

--- src/controllers/motocycleController.ts
+++ src/controllers/motocycleController.ts
@@ -9,4 +9,28 @@ export default class MotocycleController {
     const result = await this._service.create(req.body);
     return res.status(201).json(result);
   }
+
+  async read(_req: Request, res: Response) {
+    const result = await this._service.read();
+    return res.status(200).json(result);
+  }
+
+  async readOne(req: Request, res: Response) {
+    const { id } = req.params;
+    const result = await this._service.readOne(id);
+    return res.status(200).json(result);
+  }
+
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const obj = req.body;
+    const result = await this._service.update(id, obj);
+    return res.status(200).json(result);
+  }
+
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+    await this._service.delete(id);
+    return res.status(204).json();
+  }
 }
--- src/routes/motocycleRouter.ts
+++ src/routes/motocycleRouter.ts
@@ -9,5 +9,9 @@ const service = new MotocycleService(model);
 const controller = new MotocycleController(service);
 
 mRouter.post('/', (req, res) => controller.create(req, res));
+mRouter.get('/', (req, res) => controller.read(req, res));
+mRouter.get('/:id', (req, res) => controller.readOne(req, res));
+mRouter.put('/:id', (req, res) => controller.update(req, res));
+mRouter.delete('/:id', (req, res) => controller.delete(req, res));
 
 export default mRouter;
